c70076234aa00cac46b6de5f33cd00a92931f51b
thucdv4 <thucdv4@viettel.com.vn>
2024-01-17 16:16:01 +0700

    update

---
+++ src/TestFolder/TestLib.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function TestLib() {
+    return(
+        <View>
+            <Text style={{color: 'red'}}>hello</Text>
+        </View>
+    )
+}
